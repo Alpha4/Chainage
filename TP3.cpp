d5dd43d81a5fa6ce4df91d348af640b684325cdd
Alpha4 <aure.b21@gmail.com>
2012-11-26 19:30:48 +0100

    Menu
    Ecriture du menu

--- TP3.cpp
+++ TP3.cpp
@@ -7,46 +7,61 @@ using namespace chainage;
 int main()
 {
 	Chainage ch;
-	int elem, pos, choix, rech, d, f;
+	int elt, pos, choix,;
 
 	initialisation(ch);
-
-	
-
-	cout << "Que souhaitez vous faire ?" << endl;
-
 	do
 	{
 		affichage(ch);
+		cout << "**********MENU**********" << endl;
 		cout << "1 : Insertion" << endl;
-		cout << "2 : Suppression" << endl;
-		cout << "3 : Recherche" << endl;
-		cout << "6 : Afficher le chainage" << endl;
-		cout << "10 : Terminer" << endl;
+		cout << "2 : Insertion en tête" << endl;
+		cout << "3 : Insertion en queue" << endl;
+		cout << "4 : Suppression" << endl;
+		cout << "5 : Suppression en tête" << endl;
+		cout << "6 : Suppression en queue" << endl;
+		cout << "7 : Recherche" << endl;
+		cout << "8 : Afficher le chainage en sens inverse" << endl;
+		cout << "9 : Recherche du minimum" << endl;
+		cout << "10 : Supprimer le minimum" << endl;
+		cout << "11 : Insertion avant le minimum" << endl;
+		cout << "12 : Insertion après le minimum" << endl;
+		cout << "13 : Terminer" << endl;
 		cin >> choix;
-
-		cout << endl;
+		cout << "************************" << endl;
 
 		switch(choix)
 		{
 			case 1:
 				cout << "Quel élément souhaitez vous insérer ?" << endl;
-				cin >> elem; // Récupération de l'élément
+				cin >> elt; // Récupération de l'élément
 				cout << "En quelle position ? (0 pour insérer en tête)" << endl;
 				cin >> pos; // Récupération de la position
 				if (pos>=0 && pos<=ch.nb_elt)
 				{
-					insertion(ch,elem,pos);
+					insertion(ch,elt,pos);
 				}
 				else
 				{
-					cout << "Insertion impossible" << endl;
+					cout << "Position inexistante" << endl;
 				}
 			break;
-
+			
 			case 2:
+				cout << "Quel élément souhaitez vous insérer ?" << endl;
+				cin >> elt; // Récupération de l'élément
+				insertTete(ch,elt);
+			break;
+			
+			case 3:
+				cout << "Quel élément souhaitez vous insérer ?" << endl;
+				cin >> elt; // Récupération de l'élément
+				insertQueue(ch,elt);
+			break;
+
+			case 4:
 				cout << "Quelle maillon souhaitez vous supprimer :" << endl;
-				cin >> pos;
+				cin >> pos; // Récupération de la position
 				if (pos>0 && pos<ch.nb_elt)
 				{
 					suppression(ch,pos);
@@ -56,26 +71,14 @@ int main()
 					cout << "Position inexistante" << endl;
 				}
 			break;
-/*
-			case 3:
-			cout << "Quelle element souhaitez vous rechercher :" << endl;
-			cin >> elt;
-			rech = recherche(ptab, elt);
 
-			if (rech > -1)
-				cout << "Votre element est a la position " << rech+1 << endl;
-			else
-				cout << "Votre element n'existe pas." << endl;
-			affichage(ptab);
-			break;
-
-			case 6:
-			affichage(ptab);
+			case 8:
+			affichageInverse(ch));
 			break;
 
 			default:
-			finalisation(ptab);
-			break;*/
+			finalisation(ch);
+			break;
 		}
-	}while (choix > 0 && choix < 10);
+	}while (choix > 0 && choix < 13);
 }
